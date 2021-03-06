4b17297001320c98454dd3291de474cda26bd16e
Noob-Owl <69479799+Noob-Owl@users.noreply.github.com>
2021-03-06 18:27:58 +0700

    Update studentListOperations.c

--- studentListOperations.c
+++ studentListOperations.c
@@ -1,62 +1,72 @@
 #include<stdio.h>
 #include<stdint.h>
-#include<string.h>
+
 #include "studentListOperations.h"
 
 
-int checkEmpty(FILE* filePtr)
+int checkEmpty()
 {
-    int check = fgetc(filePtr);
+    FILE* fileCheck = fopen(SOURCE, "r");
+    int exitValue = 0;
+    int check = fgetc(fileCheck);
     if (check == EOF)
     {
-        return 1;
+        exitValue = 1;
     }
     else
     {
-        return 0;
+        exitValue = 0;
     }
+    return exitValue;
+    fclose(fileCheck);
 }
 
-void printList(FILE* filePtr)
+void printList()
 {
+    FILE* filePrint = fopen(SOURCE, "r");
     char buffer [STUDENT_SIZE];
-    while(!feof(filePtr))
+
+    while(!feof(filePrint))
     {
-        if (fgets(buffer, STUDENT_SIZE, filePtr) != NULL);
+        if(fgets(buffer, STUDENT_SIZE, filePrint) != NULL);
         {
-            printf ("%s\n", buffer);
+            printf ("%s", buffer);
         }
     }
+    fclose(filePrint);
 }
 
-void addStudent(FILE* filePtr)
+void addStudent()
 {
+    FILE* fileAdd = fopen(SOURCE, "a");
     int n = 0;
-    printf ("How namy student do you want to add?\n");
+    printf ("How many student do you want to add?\n");
     scanf ("%d", &n);
-    printf ("Enter students' informations\n");
+    printf ("Enter students' informations:\n");
     int k = 0;
+    
     while(k < n)
     {
         printf ("Student %d:\n", k+1);
         printf ("\tID: ");
-        scanf ("%hhd", &student.id);
+        scanf ("%hhd", &student[k].id);
         while(getchar() != '\n');
 
         printf ("\tName: ");
-        scanf ("%[^\n]s", &student.name);
+        scanf ("%[^\n]s", &student[k].name);
         while(getchar() != '\n');
 
         printf ("\tHome land: ");
-        scanf ("%[^\n]s", &student.home);
+        scanf ("%[^\n]s", &student[k].home);
         while(getchar() != '\n');
 
         printf ("\tScore: ");
-        scanf ("%f", &student.score);
+        scanf ("%f", &student[k].score);
         while(getchar() != '\n');
         
-        fseek(filePtr, k * STUDENT_SIZE, SEEK_END);
-        fprintf(filePtr, "\n%-3d%-30s%-15s%-4.1f", student.id, student.name, student.home, student.score);
+        fseek(fileAdd, k * STUDENT_SIZE, SEEK_CUR);
+        fprintf(fileAdd, "%-3d%-30s%-15s%-4.1f\n", student[k].id, student[k].name, student[k].home, student[k].score);
         k = k + 1;
     }
+    fclose(fileAdd);
 }
